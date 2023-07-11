a48d7893c3b1f5e6fe31505594ab3f2a9575cf3e
Harsh <tripathiharsh16@gmail.com>
2023-07-11 21:24:49 +0530

    duties page started and some routes initialised

---
+++ lib/screens/dutiesPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DutiesPage extends StatefulWidget {
+  const DutiesPage({super.key});
+
+  @override
+  State<DutiesPage> createState() => _DutiesPageState();
+}
+
+class _DutiesPageState extends State<DutiesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
