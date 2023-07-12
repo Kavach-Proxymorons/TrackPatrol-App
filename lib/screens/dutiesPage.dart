54317da9971717298b559634de8c17a73590eba0
Harsh <tripathiharsh16@gmail.com>
2023-07-12 20:37:22 +0530

    UI data card complted

--- lib/screens/dutiesPage.dart
+++ lib/screens/dutiesPage.dart
@@ -81,9 +81,13 @@ class _DutiesPageState extends State<DutiesPage> {
                         ),
                         Row(
                           children: <Widget>[
-                            Icon(
-                              Icons.punch_clock_outlined,
-                              color: Color(0xffBCBCBC),
+                            Image.asset(
+                              'images/time_bw.png',
+                              height: 14,
+                              width: 13,
+                            ),
+                            SizedBox(
+                              width: 5,
                             ),
                             Text(
                               '8 hours',
@@ -111,7 +115,7 @@ class _DutiesPageState extends State<DutiesPage> {
                   ],
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 10,
               ),
               Text(
@@ -121,6 +125,17 @@ class _DutiesPageState extends State<DutiesPage> {
                   color: Color(0xff0D76D3),
                 ),
               ),
+              const SizedBox(
+                height: 10,
+              ),
+              const DutyCard(
+                dutyName: "Hussian Polling Activity",
+                location: "Location",
+                timePeriod: "00:00 am - 09:00 am",
+                time: "8 hours",
+                flag: "High",
+                date: "10 April",
+              )
             ],
           ),
         ),
@@ -129,11 +144,172 @@ class _DutiesPageState extends State<DutiesPage> {
   }
 }
 
-class DutyCard extends StatelessWidget {
-  const DutyCard({super.key});
+class DutyCard extends StatefulWidget {
+  const DutyCard(
+      {super.key,
+      required this.dutyName,
+      required this.location,
+      required this.timePeriod,
+      required this.time,
+      required this.flag,
+      required this.date});
+
+  final String dutyName;
+  final String location;
+  final String timePeriod;
+  final String time;
+  final String flag;
+  final String date;
 
   @override
+  State<DutyCard> createState() => _DutyCardState();
+}
+
+class _DutyCardState extends State<DutyCard> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Stack(
+      children: [
+        Image.asset('images/1.png'),
+        Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                widget.dutyName,
+                style: GoogleFonts.poppins(
+                    fontSize: 18, fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: <Widget>[
+                  Image.asset(
+                    'images/loc.png',
+                    height: 18,
+                    width: 12,
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    widget.location,
+                    style: GoogleFonts.poppins(fontSize: 15),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: <Widget>[
+                  Image.asset(
+                    'images/timePeriod.png',
+                    height: 18,
+                    width: 12,
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    widget.timePeriod,
+                    style: GoogleFonts.poppins(fontSize: 15),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: <Widget>[
+                  Image.asset(
+                    'images/time.png',
+                    height: 18,
+                    width: 12,
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    widget.time,
+                    style: GoogleFonts.poppins(fontSize: 15),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              FlagContainer(flag: 'High', flagColor: Colors.red)
+            ],
+          ),
+        ),
+        const Positioned(
+            top: 120,
+            left: 300,
+            child: DateContainer(
+              date: '10 April',
+            ))
+      ],
+    );
+  }
+}
+
+class FlagContainer extends StatefulWidget {
+  const FlagContainer({
+    super.key,
+    required this.flag,
+    required this.flagColor,
+  });
+  final String flag;
+  final Color flagColor;
+  @override
+  State<FlagContainer> createState() => _FlagContainerState();
+}
+
+class _FlagContainerState extends State<FlagContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 25,
+      width: 82,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: widget.flagColor,
+      ),
+      child: Center(
+          child: Text(
+        widget.flag,
+        style: GoogleFonts.poppins(color: Colors.white),
+      )),
+    );
+  }
+}
+
+class DateContainer extends StatefulWidget {
+  const DateContainer({super.key, required this.date});
+  final String date;
+  @override
+  State<DateContainer> createState() => _DateContainerState();
+}
+
+class _DateContainerState extends State<DateContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      height: 62,
+      width: 62,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10), color: Color(0xff0d76d3)),
+      child: Center(
+          child: Text(
+        textAlign: TextAlign.center,
+        softWrap: true,
+        widget.date,
+        style: GoogleFonts.poppins(color: Colors.white),
+      )),
+    );
   }
 }
